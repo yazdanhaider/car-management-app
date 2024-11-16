fd21e1ee24383db561931502f82e67be0af4729d
Yazdan Haider <yazdanhaider007@gmail.com>
2024-11-17 00:24:24 +0530

    depl: fixed cors

--- server/src/index.js
+++ server/src/index.js
@@ -17,8 +17,6 @@ app.use(cookieParser());
 app.use(cors({
   origin: true,
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
 }));
 
 // Routes
